362e8f7625e4da8d1a04404d6fb937438967a5fd
Тарас Гавриляк <taras.havryliak@gmail.com>
2020-10-20 21:54:55 +0300

    implemented random cardColor function

--- app/src/main/java/com/example/pokedex/MainAdapter/PokemonsListAdapter.kt
+++ app/src/main/java/com/example/pokedex/MainAdapter/PokemonsListAdapter.kt
@@ -1,6 +1,8 @@
 package com.example.pokedex.MainAdapter
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,8 +13,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.pokedex.Model.PokeData
 import com.example.pokedex.R
 import com.squareup.picasso.Picasso
+import kotlin.random.Random
 
 class PokemonListAdapter : ListAdapter<PokeData, PokemonListAdapter.PokemonViewHolder>(PokemonDiffCallback()) {
+    private lateinit var context: Context
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonViewHolder {
         return PokemonViewHolder(
@@ -21,7 +25,14 @@ class PokemonListAdapter : ListAdapter<PokeData, PokemonListAdapter.PokemonViewH
     }
 
     override fun onBindViewHolder(holder: PokemonViewHolder, position: Int) {
+        context = holder.itemView.context
+        holder.itemView.setBackgroundColor(setRandomCardColor())
         holder.bindTo(getItem(position))
+    }
+
+    private fun setRandomCardColor(): Int {
+        val coloredList: IntArray = context.resources.getIntArray(R.array.colors)
+        return coloredList[Random.nextInt(1, coloredList.size)]
     }
 
     class PokemonViewHolder(view: View) : RecyclerView.ViewHolder(view) {
